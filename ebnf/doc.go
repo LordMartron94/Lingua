c7b7c9c1fe782bcb36b5615375e9cd3276e72a81
Matthew van der Hoorn <md.career@protonmail.com>
2026-05-01 10:29:20 +0200

    remove ebnf lang
    inclusion in LSpec proved more complex than anticipated with little benefit; so support is dropped

--- ebnf/doc.go
+++
@@ -1,7 +0,0 @@
-/*
-Package ebnf defines the syntax for EBNF.
-*/
-package ebnf
-
-//go:generate go run ../internal/generation/bootstrap/main.go -spec=ebnf.lspec
-//go:generate go run generate/gen.go
